352821b81db3ab3eafa083efd6452be9ae142b9e
Shirllie-Apiyo <apraise320@gmail.com>
2022-01-27 14:25:15 +0300

    login user

--- routes.js
+++ routes.js
@@ -225,4 +225,43 @@ router.post("/register-user",(req,res, next)=>{
     });
 });
 
+
+
+// sign-in route
+router.post("/signin", (req,res, next)=>{
+let getUser;
+userSchema.findOne({
+    email:req.body.email
+}).then(user =>{
+    if(!user){
+        return res.status(401).json({
+            message:"Email does not exist"
+        });
+    }
+    getUser = user;
+    return bcrypt.compare(req.body.password, user.password);
+}).then(response =>{
+    if(!response){
+        return res.status(401).json({
+            message:"Authentication failed"
+        });
+    }
+    let jwtToken = jwt.sign({
+        email:getUser.email,
+        userId:getUser._id
+    },"longer-secret-is-better",{
+        expiresIn:"1h"
+    });
+    res.status(200).json({
+        token:jwtToken,
+        expiresIn:3600,
+        msg:getUser
+    });
+}).catch(err =>{
+    return res.status(401).json({
+        message:"Authentication Failed"
+    });
+});
+});
+
 module.exports = router;
